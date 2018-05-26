f30212d3b6f64b05ad8f03e93cbac6ae2744bdc5
Sage Vogt <thurmanvogt@gmail.com>
2018-05-26 15:37:14 -0600

    Add necessary containers with file structure

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,15 +1,36 @@
 import React, { Component } from 'react';
 import './App.css';
 import MapContainer from './../MapContainer/MapContainer';
+import { Route, Redirect, Switch, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <MapContainer />
+        <Switch>
+          <Route 
+            exact path="/" 
+            component={DestinationsContainer}
+          />
+          <Route 
+            exact path="/signin" 
+            component={SigninContainer}
+          />
+          <Route 
+            exact path="/rides" 
+            component={RidesContainer}
+          />
+          <Route 
+            exact path="/profile" 
+            component={ProfileContainer}
+          />
+        </Switch>
       </div>
     );
   }
 }
 
-export default App
+export default withRouter(App)
---
+++ src/containers/DestinationsContainer/DestinationsContainer.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+class DestinationsContainer extends Component {
+  constructor(props) {
+    super();
+  }
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export const mapStateToProps = (state) => ({
+
+})
+
+export const mapDispatchToProps = (dispatch) => ({
+
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(DestinationsContainer);
