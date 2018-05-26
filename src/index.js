1c4f9b7c70daad7a6aafc0e5e2d535f5f472e903
Sage Vogt <thurmanvogt@gmail.com>
2018-05-26 12:05:19 -0600

    Add index.js structure with imports

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
+import { BrowserRouter as Router} from 'react-router-dom';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from '../src/reducers/index';
+import App from './containers/App/App';
 import './index.css';
-import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const store = createStore(rootReducer, devTools);
+
+render(
+  <Provider store={store} >
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+);
