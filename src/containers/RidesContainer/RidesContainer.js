64a8090e22ac451ea59a1eab37d2e8347e1257fc
Sage Vogt <thurmanvogt@gmail.com>
2018-05-30 14:22:41 -0600

    First popover settings

--- src/containers/RidesContainer/RidesContainer.js
+++ src/containers/RidesContainer/RidesContainer.js
@@ -8,7 +8,7 @@ import { addRides } from './../../actions/rides';
 import { addCurrentLocation } from './../../actions/currentLocation';
 import * as cleaner from './../../cleaners/cleaners';
 import OfferContainer from './../OfferContainer/OfferContainer';
-
+import RidePopoverComponent from './../../components/RidePopoverComponent/RidePopoverComponent';
 import './RidesContainer.css';
 export class RidesContainer extends Component {
   constructor(props) {
@@ -68,6 +68,7 @@ export class RidesContainer extends Component {
             <div>car model: {ride.car_type}</div>
             <div>date: {ride.date}</div>
             <div>time: {ride.time}</div>
+            <RidePopoverComponent />
           </article>
         );
     }))
