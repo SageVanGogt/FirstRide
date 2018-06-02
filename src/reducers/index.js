1035127d810d279607ac51e2fda4d8d7ba9f7638
Sage Vogt <thurmanvogt@gmail.com>
2018-06-02 14:54:16 -0600

    Add userRidesReducer to rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -7,16 +7,18 @@ import ridesReducer from './ridesReducer';
 import errorReducer from './errorReducer';
 import profileReducer from './profileReducer';
 import ridesAccountedReducer from './rideAccountedReducer';
+import userRidesReducer from './userRidesReducer';
 
 const rootReducer = combineReducers({
- user: userReducer,
- rides: ridesReducer,
- currentLocation: currentLocationReducer,
- destination: destinationReducer,
- pickupLocations: pickupReducer,
- profile: profileReducer,
- error: errorReducer,
- ridesAccounted: ridesAccountedReducer
+  user: userReducer,
+  rides: ridesReducer,
+  currentLocation: currentLocationReducer,
+  destination: destinationReducer,
+  pickupLocations: pickupReducer,
+  profile: profileReducer,
+  error: errorReducer,
+  ridesAccounted: ridesAccountedReducer,
+  userRides: userRidesReducer
 });
 
 export default rootReducer;
