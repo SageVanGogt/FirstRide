7f9cd9aa55b8b58de4e50984756ba7d7723debed
Sage Vogt <thurmanvogt@gmail.com>
2018-06-02 18:17:10 -0600

    Add pickupReducer that toggles isShowing state

--- src/reducers/pickupReducer.test.js
+++ src/reducers/pickupReducer.test.js
@@ -1,13 +1,9 @@
 import pickupReducer from './pickupReducer';
 
 describe('pickupReducer', () => {
-  let initialState;
-
-  beforeEach(() => {
-    initialState = [];
-  })
 
   it('should return a state with pickup locations', () => {
+    let initialState = [];
     let mockAction = {
       type: 'ADD_PICKUPS',
       pickups: [{}, {}]
@@ -15,6 +11,37 @@ describe('pickupReducer', () => {
     let expected = [{}, {}];
     let actual = pickupReducer(initialState, mockAction);
     
+    expect(actual).toEqual(expected);
+  })
+
+  it('should return a state with a toggled show pickup', () => {
+    let initialState = [
+      {
+        id: 1,
+        isShowing: false
+      }, 
+      {
+        id: 2,
+        isShowing: false
+      }];
+    let mockAction = {
+      type: "TOGGLE_SHOWING",
+      pickup: {
+        id: 1,
+        isShowing: false
+      }
+    };
+    let expected = [
+      {
+        id: 1,
+        isShowing: true
+      }, 
+      {
+        id: 2,
+        isShowing: false
+      }]
+    let actual = pickupReducer(initialState, mockAction);
+
     expect(actual).toEqual(expected);
   })
 })
