08c153e3d11e8f44bfd0223adc38f22b3a4b9dce
Sage Vogt <thurmanvogt@gmail.com>
2018-06-05 12:58:32 -0600

    Add premap instructions telling user to enter an address

--- src/containers/MapContainer/MapContainer.js
+++ src/containers/MapContainer/MapContainer.js
@@ -28,15 +28,24 @@ export class MapContainer extends Component {
   render() {
     return (
       <div className="map-container">
-        <MapComponent
-          position={this.props.currentLocation}
-          googleMapURL={mapUrl}
-          rides={this.props.rides}
-          markerCoords={this.props.pickupLocations}
-          toggleShowing={this.toggleShowing}
-          loadingElement={this.loadingElement()}
-          containerElement={this.containerElement()}
-          mapElement={this.mapElement()} />
+        {
+          this.props.currentLocation.lat ? 
+            <MapComponent
+              position={this.props.currentLocation}
+              googleMapURL={mapUrl}
+              rides={this.props.rides}
+              markerCoords={this.props.pickupLocations}
+              toggleShowing={this.toggleShowing}
+              loadingElement={this.loadingElement()}
+              containerElement={this.containerElement()}
+              mapElement={this.mapElement()} /> :
+            <div className="map-instructions">
+              <h4 className="map-text">
+                Input your current address to find pickup pickup
+                locations near you
+              </h4>
+            </div>
+        }
       </div>
     );
   }
